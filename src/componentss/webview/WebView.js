e496d603d0dfecdcb8c972d50a40306eff511d4c
raisrazzaq <raisrazzaq@gmail.com>
2024-04-26 12:10:55 +0500

    set url addres of the WebView

---
+++ src/componentss/webview/WebView.js
@@ -0,0 +1,16 @@
+// import {SafeAreaView} from 'react-native';
+// import React from 'react';
+// import {WebView} from 'react-native-webview';
+
+// const WebViewComponent = () => {
+//   return (
+//     <SafeAreaView style={{flex: 1}}>
+//       <WebView
+//         source={{uri: 'https://www.w3schools.com/REACT/react_getstarted.asp'}}
+//         style={{flex: 1}}
+//       />
+//     </SafeAreaView>
+//   );
+// };
+
+// export default WebViewComponent;
